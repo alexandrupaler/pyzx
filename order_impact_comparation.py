5249557dbaf52936f2ba17011a7d6c1722fd9e6c
VoicuTomut <tomutvoicuandrei@gmail.com>
2022-05-25 14:08:07 +0300

    checks in order contraction

--- order_impact_comparation.py
+++ order_impact_comparation.py
@@ -21,7 +21,7 @@ random.seed(1)
 # circuit = pyzx.generate.CNOT_HAD_PHASE_circuit_mixt(qubits, depth, p_had=0.5, p_cnot=0.5)
 
 # Load circuit_from file
-circuit_path ="C:/Users/tomut/Documents/GitHub/pyzx/circuits/Arithmetic_and_Toffoli"+"/tof_3_after_light"#"/gf2^4_mult_before"
+circuit_path ="C:/Users/tomut/Documents/GitHub/pyzx/circuits/Arithmetic_and_Toffoli"+"/mod_adder_1024_tpar.qc"#"/gf2^4_mult_before"
 circuit = pyzx.Circuit.load(circuit_path)
 qubits = circuit.qubits
 print("nr_qubits:", qubits)
@@ -45,31 +45,31 @@ baseline_circ, baseline_graph = baseline_pipeline.load(circuit)
 mansikka_circ, mansikka_graph = mansikka_pipeline.load(circuit)
 simple_circ, simple_graph = simple_pipeline.load(circuit)
 
-# matrix_0 = baseline_pipeline.extract(baseline_graph)
-# matrix_1 = mansikka_pipeline.extract(mansikka_graph)
-# matrix_2 = mansikka_pipeline.extract(simple_graph)
-#
-# print("Equals matrix_0=matrix_1: ", np.allclose(matrix_0, matrix_1))
-# print("Equals matrix_0=matrix_2: ", np.allclose(matrix_0, matrix_2))
+#matrix_0 = baseline_pipeline.extract(baseline_graph)
+matrix_1 = mansikka_pipeline.extract(mansikka_graph)
+matrix_2 = simple_pipeline.extract(simple_graph)
+
+print("Equals matrix_0=matrix_1: ", np.allclose(matrix_0, matrix_1))
+print("Equals matrix_0=matrix_2: ", np.allclose(matrix_0, matrix_2))
 
 
 #@profile()
-def get_memeo():
-    print("## Time performace##:\n")
-
-    initial_state = np.zeros((2 ** qubits,), dtype=np.complex64)
-    initial_state[0] = 1
-
-    result_0 = baseline_pipeline.evaluate_with_mcsim(initial_state, baseline_graph)
-    del(result_0)
-    gc.collect()
-    result_1 = mansikka_pipeline.evaluate_with_mcsim(initial_state, mansikka_graph)
-    result_2 = simple_pipeline.evaluate_with_mcsim(initial_state, simple_graph)
-
-    print("\n####baseline:####\n", baseline_pipeline.timing)
-    print("\n####mansika:####\n", mansikka_pipeline.timing)
-    print("\n####simple:####\n", simple_pipeline.timing)
-
-    print("-Done-")
-get_memeo()
+# def get_memeo():
+#     print("## Time performace##:\n")
+#
+#     initial_state = np.zeros((2 ** qubits,), dtype=np.complex64)
+#     initial_state[0] = 1
+#
+#     result_0 = baseline_pipeline.evaluate_with_mcsim(initial_state, baseline_graph)
+#     del(result_0)
+#     gc.collect()
+#     result_1 = mansikka_pipeline.evaluate_with_mcsim(initial_state, mansikka_graph)
+#     result_2 = simple_pipeline.evaluate_with_mcsim(initial_state, simple_graph)
+#
+#     print("\n####baseline:####\n", baseline_pipeline.timing)
+#     print("\n####mansika:####\n", mansikka_pipeline.timing)
+#     print("\n####simple:####\n", simple_pipeline.timing)
+#
+#     print("-Done-")
+# get_memeo()
 
